3d280691d887b3c50de19310a089627806250e42
mishamyrt <misha@myrt.co>
2026-03-24 11:10:13 +0300

    chore: fix compiledb path

--- scripts/pio_db_include_toolchain.py
+++ scripts/pio_db_include_toolchain.py
@@ -6,4 +6,4 @@ Import("env")
 env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)
 
 # # override compilation DB path
-env.Replace(COMPILATIONDB_PATH=os.path.join("$BUILD_DIR", "compile_commands.json"))
+env.Replace(COMPILATIONDB_PATH=os.path.join("./.pio/build", "compile_commands.json"))
